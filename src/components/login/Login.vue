eb62a0e39049f649d0e539ccbc9dd6bb7da2b48a
Josmadelmo Davi <josmadelmodavi@gmail.com>
2020-05-30 14:21:25 -0300

    Primeiro commit

---
+++ src/components/login/Login.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <h1>Login</h1>
+    <div>
+        <label for="username">Username:</label>
+        <input type="text" v-model="username" autofocus="autofocus">
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" autocomplete="new-password">
+    </div>
+    <button @click="doLogin(username, password)">Login</button>
+</div>
+</template>
+
+<script>
+import { doLoginApi } from "../../services/api.js";
+
+export default {
+    data() {
+        return {
+            username: "registered_user1",
+            password: "111"
+        }
+    },
+    methods: {
+        doLogin(username, password){
+            doLoginApi(username,password).then(
+                result => console.log(result.data), 
+                error => console.log(error.response.data.error_message)
+            );
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+// ./src/routes.js
+import Login from './components/login/Login.vue'
+
+export const routes = [
+    {
+        name: 'login',
+        path: '/login',
+        component: Login
+    }
+];
